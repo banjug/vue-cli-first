e2a7d33557d942bf970bb12f9ba276226f840735
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-23 13:42:41 +0100

    add hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="jumbotron">
+    <img src="../assets/1.jpg" alt="">
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.jumbotron {
+    width: 80%;
+    height: 500px;
+    overflow: hidden;
+    margin: 0 auto;
+    position: relative;
+}
+img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+
+h2 {
+    position: absolute;
+    bottom: 0;
+    right: 25px;
+    color: white;
+}
+
+</style>
